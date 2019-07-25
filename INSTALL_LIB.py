74c150264aa411d9d6f52ad421168f0d8ff75126
denisxab <47678176+denisxab@users.noreply.github.com>
2019-07-25 23:39:35 +0300

    Add files via upload

---
+++ INSTALL_LIB.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+
+
+def insatll_lib(name:str):
+	command = f'pip install {name}'
+	cash_text = re.findall(re.compile('[^\n-- ]+'),os.popen(command).read())
+
+	if cash_text[0] == 'Requirement':
+		return True
+
+	elif cash_text[0] == 'Collecting':
+		return False
+
+def chek_libs(list_libr:dict):
+	ASD = os.popen('pip list').read()
+	cash_text = re.findall(re.compile('[^\n-- ]+'), ASD)
+	return_list = {'ERROR':[]}
+
+	for x in list_libr.items():
+
+		if x[0] in cash_text[2:]:
+			continue
+			
+		if not x[0] in cash_text[2:]:
+			res = (insatll_lib(f'{x[1]}'))
+			if not res:
+				return_list['ERROR'].append({x[0]:x[1]})
+
+	os.system('cls')
+	if return_list['ERROR'] == []:
+		return True
+
+	if return_list['ERROR'] != []:
+		return return_list
+
+
+list_libr = {
+			'pyperclip':'pyperclip',
+			'selenium':'selenium',
+			'requests':'requests',
+			'win32api':'pypiwin32',
+			'pypiwin32':'pypiwin32',
+			'Pillow':'Pillow',
+			'mss':'mss',
+			'opencv-python':'opencv-python',
+			'numpy':'numpy',
+			'pytesseract':'pytesseract',
+			}
+
+
+print(chek_libs(list_libr))
+input()
