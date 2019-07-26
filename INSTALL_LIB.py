a7106b72cf7a703be24ebd3e84b1afb0066b9ee7
denisxab <47678176+denisxab@users.noreply.github.com>
2019-07-26 09:42:51 +0300

    Add files via upload

--- INSTALL_LIB.py
+++ INSTALL_LIB.py
@@ -2,8 +2,9 @@
 import os
 import re
 import pip._internal.utils.misc as pip
+import sys
 
-
+#########################################
 def insatll_lib(name: str):
     command = f'pip install {name}'
     cash_text = re.findall(re.compile('[^\n-- ]+'), os.popen(command).read())
@@ -40,9 +41,12 @@ def chek_libs(list_libr: dict):
 
     if return_list['ERROR'] != []:
         return return_list
+#########################################
+
+# Заполнить list_libr
 
 
-def main():
+def main_64():  # Windows x64
 
     list_libr = {
         'pyperclip': 'pyperclip',
@@ -59,6 +63,43 @@ def main():
     return chek_libs(list_libr)
 
 
+def main_32():  # Windows x32
+    list_libr = {}
+    return chek_libs(list_libr)
+
+
+def main_linux():  # Linux
+    list_libr = {}
+    return chek_libs(list_libr)
+
+
+def main_darwin():  # MacOS
+    list_libr = {}
+    return chek_libs(list_libr)
+
+
 if __name__ == '__main__':
-    print(main())
+
+    """
+    Устанавливайте разные библиотеки для разных платформ
+    ----------------------------------------------------
+    Install different libraries for different platforms
+    """
+
+    if sys.platform == 'win32':  # Windows
+        import platform
+
+        if platform.architecture()[0] == '64bit':  # Windows x64
+            res = main_64()
+
+        if platform.architecture()[0] == '32bit':  # Windows x32
+            res = main_32()
+
+    elif sys.platform == 'linux':  # Linux
+        res = main_linux()
+
+    elif sys.platform == 'darwin':  # MacOS
+        res = main_darwin()
+
+    print(res)
     input()
