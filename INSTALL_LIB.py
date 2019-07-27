f71c4df299e703098c7d760d65616a4676db0986
denisxab <47678176+denisxab@users.noreply.github.com>
2019-07-27 22:36:55 +0300

    Add files via upload

--- INSTALL_LIB.py
+++ INSTALL_LIB.py
@@ -4,11 +4,12 @@ import re
 import pip._internal.utils.misc as pip
 import sys
 
+
 #########################################
 
 
 def insatll_lib(name: str):
-    if not isinstance(name, str): return False
+    if not isinstance(name, str):return False
     #____________________________________________#
     command = f'pip install {name}'
     cash_text = re.findall(re.compile('[^\n-- ]+'), os.popen(command).read())
@@ -21,7 +22,7 @@ def insatll_lib(name: str):
 
 
 def chek_libs(list_libr: dict):
-    if not isinstance(list_libr, dict): return False
+    if not isinstance(list_libr, dict):return False
     #____________________________________________#
     installed_packages_list = sorted(
         ["%s" % i.key for i in pip.get_installed_distributions()])
@@ -43,38 +44,46 @@ def chek_libs(list_libr: dict):
 #########################################
 
 
+def INSTALL_LIB(list_libr: dict):
 
-def INSTALL_LIB(list_libr:dict):
-    if not isinstance(list_libr, dict): return False
+    # print(help(INSTALL_LIB.INSTALL_LIB))
     """
     {
-    'all': {'requests': 'requests'},  # all для всех платформ
-    '64bit': {'requests': 'requests'}, 
-    '32bit': {'requests': 'requests'}, 
-    'linux': {'requests': 'requests'}, 
-    'darwin': {'requests': 'requests'}
+    'all': {'requests': 'requests'},   # all для всех платформ
+    '64bit': {'requests': 'requests'}, # Windows 64bit
+    '32bit': {'requests': 'requests'}, # Windows 32bit
+    'linux': {'requests': 'requests'}, # Linux
+    'darwin': {'requests': 'requests'} # MacOS
     }
     """
+
+    if not isinstance(list_libr, dict):return False
+
     #____________________________________________#
     try:
-        if list_libr['all']: 
-            bit64=bit32=linux=darwin=list_libr['all']
+        if list_libr['all']:
+            bit64 = bit32 = linux = darwin = list_libr['all']
 
     except KeyError:
-        try: bit64 = list_libr['64bit']
-        except KeyError: return 'Error 64bit'
+        try:
+            bit64 = list_libr['64bit']
+        except KeyError:
+            return 'Error 64bit'
 
-        try:bit32 = list_libr['32bit']
-        except KeyError: return 'Error 32bit'
+        try:
+            bit32 = list_libr['32bit']
+        except KeyError:
+            return 'Error 32bit'
 
-        try:linux = list_libr['linux']
-        except KeyError: return 'Error linux'
+        try:
+            linux = list_libr['linux']
+        except KeyError:
+            return 'Error linux'
 
-        try:darwin = list_libr['darwin']
-        except KeyError: return 'Error darwin'
-
-
-
+        try:
+            darwin = list_libr['darwin']
+        except KeyError:
+            return 'Error darwin'
 
     if sys.platform == 'win32':  # Windows
         import platform
@@ -94,14 +103,12 @@ def INSTALL_LIB(list_libr:dict):
     return res
 
 
-
-
 if __name__ == '__main__':
     print(INSTALL_LIB({
-        'all': {'requests': 'requests'},  
-        '64bit': {'requests': 'requests'}, 
-        '32bit': {'requests': 'requests'}, 
-        'linux': {'requests': 'requests'}, 
+        'all': {'requests': 'requests'},
+        '64bit': {'requests': 'requests'},
+        '32bit': {'requests': 'requests'},
+        'linux': {'requests': 'requests'},
         'darwin': {'requests': 'requests'},
-        }))
+    }))
     input()
